9c59a1ba25e1846b1885b13e87a2c17225b6c1a8
yaoxj <2604222937@qq.com>
2017-05-27 09:59:42 +0800

    test commit

--- src/main/java/com/demo/spring/controller/DemoController.java
+++ src/main/java/com/demo/spring/controller/DemoController.java
@@ -3,6 +3,11 @@ package com.demo.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * spring mvc controller test
+ * @author yaoxj
+ * @time 2017年5月27日上午9:53:29
+ */
 @Controller
 public class DemoController {
 
